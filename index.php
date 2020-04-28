fa0084c28d26da41bbe8fb33d2fe6341730746fa
jfieldss <48221958+jfieldss@users.noreply.github.com>
2020-04-27 20:50:07 -0400

    Update index.php

--- index.php
+++ index.php
@@ -42,5 +42,5 @@ fclose($input);
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
   </body>
-<p align="right"><a href="create.php" class="btn btn-primary btn-lg active" role="button">Submit your own car!</a></p>
+<p align="center"><a href="create.php" class="btn btn-primary btn-lg active" role="button">Submit your own car!</a></p>
 </html>
